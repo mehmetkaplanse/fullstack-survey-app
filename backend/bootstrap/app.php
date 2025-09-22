2ab34750bbf5b1afdf6376b6a4e94e624570b99a
Mehmet Kaplan <87800510+mehmetkaplanse@users.noreply.github.com>
2025-09-22 14:29:55 +0300

    fix: test cors

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\LogApiRequests;
 use App\Http\Middleware\CorsMiddleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,9 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //$middleware->append(LogApiRequests::class);
-        $middleware->api(prepend: [
-            CorsMiddleware::class,
-        ]);
+        $middleware->append(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
