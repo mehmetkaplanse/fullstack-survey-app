8af3b5bed058fa51552ac0b696e66ef7ff39d747
Mehmet Kaplan <87800510+mehmetkaplanse@users.noreply.github.com>
2025-09-23 11:52:51 +0300

    test

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,15 +3,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:5175',
-        'http://127.0.0.1:5175',
-        'https://fullstack-survey-app.onrender.com',
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
 
